6208c9c9ae8b2bd0f3b48d5493ed1c06c2c976d5
Sebastián Mejía Ríos <sebastianmr6@gmail.com>
2024-12-03 14:18:33 -0500

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+    site: 'https://sebastianmr6.github.io',
+    base: 'tutorial-sig',
+  })
